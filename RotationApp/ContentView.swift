20591573221668e0f18ed28db7ea6fc47b6329a3
prasanth <pgogineni@gmail.com>
2020-07-19 07:48:57 -0400

    Initial Commit

---
+++ RotationApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  RotationApp
+//
+//  Created by Prasanth Gogineni on 7/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
